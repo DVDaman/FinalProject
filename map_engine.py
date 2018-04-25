86c9ecc3fc9fe2caa0c186d641680dc696c6248c
DVDaman <38299245+DVDaman@users.noreply.github.com>
2018-04-25 13:32:58 -0700

    map file

--- map_engine.py
+++ map_engine.py
@@ -1,7 +1,16 @@
 import pygame
-import classes
+from classes import *
 
+room_settings = RoomSettings()
 class Map_Engine():
-    def load_map(file):
+    def __init__(self):
+        self.a = "map"
+    def load_map(self, file):
         with open(file, "r") as mapfile:
             map_data = mapfile.read()
+        #Read tile data
+        map_data = map_data.split("-") #Splits the tiles at the -'s
+        map_size = map_data[len(map_data)-1] #Map dimentions
+        map_data.remove(map_size)
+        map_size = map_size.split(",")
+        map_size[0] = int(map_size[0])*room_settings.screen_tile
